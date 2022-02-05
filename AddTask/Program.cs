53109a83a9a22bf12dbef6c8941f64eb4dd49487
JuliaTrushkova <kosyuska@yandex.ru>
2022-02-05 20:55:48 +0300

    feat: add AddTask
    calculate days till nearest birthday

---
+++ AddTask/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Teacher!");
+
+Console.WriteLine("Введите дату своего рождения:");
+string? dateOfBirthString = Console.ReadLine();
+
+if (!string.IsNullOrEmpty(dateOfBirthString))
+{
+    if (DateTime.TryParse(dateOfBirthString, out DateTime dateOfBirth))
+    {
+        //Проверка в этом году будет день рождения или в следующем
+        int year = DateTime.Now.Year;
+        if ((dateOfBirth.Month < DateTime.Now.Month) ||
+            (dateOfBirth.Month == DateTime.Now.Month && dateOfBirth.Day < DateTime.Now.Day))
+            year++;
+
+        //Создание ближайшей даты дня рождения
+        DateTime dateOfBirthThisYear = new DateTime(year, dateOfBirth.Month, dateOfBirth.Day);
+        Console.WriteLine($"Ближайший день рождения будет {dateOfBirthThisYear.ToString()}");
+
+        //Расчет количества дней до ближайшей даты дня рождения
+        TimeSpan timeSpanBirthday = dateOfBirthThisYear - DateTime.Now;        
+        Console.WriteLine($"До дня рождения осталось {timeSpanBirthday.Days} дней");
+    }
+}
