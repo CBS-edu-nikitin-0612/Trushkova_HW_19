20aed136b76f111eb4f0403927dc95bd2b0c03d8
JuliaTrushkova <kosyuska@yandex.ru>
2022-02-05 20:58:06 +0300

    fix AddTask

--- AddTask/Program.cs
+++ AddTask/Program.cs
@@ -1,25 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Teacher!");
-
-Console.WriteLine("Введите дату своего рождения:");
-string? dateOfBirthString = Console.ReadLine();
-
-if (!string.IsNullOrEmpty(dateOfBirthString))
+while (true)
 {
-    if (DateTime.TryParse(dateOfBirthString, out DateTime dateOfBirth))
+    Console.WriteLine("\nВведите дату своего рождения:");
+    string? dateOfBirthString = Console.ReadLine();
+
+    if (!string.IsNullOrEmpty(dateOfBirthString))
     {
-        //Проверка в этом году будет день рождения или в следующем
-        int year = DateTime.Now.Year;
-        if ((dateOfBirth.Month < DateTime.Now.Month) ||
-            (dateOfBirth.Month == DateTime.Now.Month && dateOfBirth.Day < DateTime.Now.Day))
-            year++;
+        if (DateTime.TryParse(dateOfBirthString, out DateTime dateOfBirth))
+        {
+            //Проверка в этом году будет день рождения или в следующем
+            int year = DateTime.Now.Year;
+            if ((dateOfBirth.Month < DateTime.Now.Month) ||
+                (dateOfBirth.Month == DateTime.Now.Month && dateOfBirth.Day < DateTime.Now.Day))
+                year++;
 
-        //Создание ближайшей даты дня рождения
-        DateTime dateOfBirthThisYear = new DateTime(year, dateOfBirth.Month, dateOfBirth.Day);
-        Console.WriteLine($"Ближайший день рождения будет {dateOfBirthThisYear.ToString()}");
+            //Создание ближайшей даты дня рождения
+            DateTime dateOfBirthThisYear = new DateTime(year, dateOfBirth.Month, dateOfBirth.Day);
+            Console.WriteLine($"Ближайший день рождения будет {dateOfBirthThisYear.ToString()}");
 
-        //Расчет количества дней до ближайшей даты дня рождения
-        TimeSpan timeSpanBirthday = dateOfBirthThisYear - DateTime.Now;        
-        Console.WriteLine($"До дня рождения осталось {timeSpanBirthday.Days} дней");
+            //Расчет количества дней до ближайшей даты дня рождения
+            TimeSpan timeSpanBirthday = dateOfBirthThisYear - DateTime.Now;
+            Console.WriteLine($"До дня рождения осталось {timeSpanBirthday.Days} дней");
+        }
+        else Console.WriteLine("Дата введена неправильно");
     }
 }
